291113a85b0911d27aa6f11e460c74710b45f38a
Isabelle <isa.bidou@gmail.com>
2022-05-25 17:24:32 +0200

    function complement  - hooks

---
+++ src/CounterHooks.js
@@ -0,0 +1,16 @@
+import React, { useState }from "react";
+export default function CounterHooks({initialCount}) {
+    const [count, setCount] = useState(initialCount)
+    return (
+        <>
+            <button onClick={ () =>  setCount(prevCount => prevCount-1)}>-</button>
+            <span>{count}
+            </span>
+            <button onClick={ ()=>  setCount( prevCount => prevCount+1)}>+</button>
+      
+        </>
+
+    )
+        
+    
+}
